6e3583aab83e74dc07d8c0cc48a4d7045cdf4697
Hyowon83 <aile1383@gmail.com>
2021-08-30 17:36:11 +0900

    컨트롤러 수정

--- hotel/src/main/java/com/hotel/app/HomeController.java
+++ hotel/src/main/java/com/hotel/app/HomeController.java
@@ -1,14 +1,15 @@
 package com.hotel.app;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,107 +22,73 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "choose";
-	}
-	
-	@RequestMapping("/tologin")
-	public String doLogin() {
-		return "login";
-	}
-	
-	@RequestMapping(value = "/checknewbie", method = RequestMethod.POST)
-	public String checkNewbie(HttpServletRequest hsr, Model model) {
-		String uname = hsr.getParameter("username");
-		String uid = hsr.getParameter("userid");
-		model.addAttribute("name", uname);
-		model.addAttribute("id", uid);
-		return "checknewbie";
-	}
-	
-	@RequestMapping("/login")
-	public String doInfo(HttpServletRequest hsr, Model model) {
-		String strPath = hsr.getParameter("path");
-		if(strPath.equals("login")) {
-			return "booking";
-		} else if(strPath.equals("choose")) {
-			return "choose";
-		} else {
-			return "choose";
-		}
-	}
-	
-	@RequestMapping(value="/check_user")
-	public String check_user(HttpServletRequest hsr, Model model) {
-	      String userid=hsr.getParameter("userid");
-	      String userpw=hsr.getParameter("userpw");
-	      System.out.println("userid:"+userid+"userpw="+userpw);
-	       HttpSession session=hsr.getSession();
-	       session.setAttribute("loginid", userid);
-	       return "redirect:/booking";
-	}
-   
-	@RequestMapping(value="/booking",method=RequestMethod.GET)
-	public String booking(HttpServletRequest hsr) {
-	      return "booking";
-	}
-	
-	@RequestMapping("/room")
-	public String room() {
-		return "room";
-	}
-	
-	@RequestMapping("/logout")
-	public String logout(HttpServletRequest hsr) {
-	      HttpSession session=hsr.getSession();
-	      session.invalidate();
-	      return "choose";
-	   }
-	
-	@RequestMapping("/selected")
-	public String doJob(HttpServletRequest hsr, Model model) {
-		String strPath = hsr.getParameter("path");
-		if(strPath.equals("login")) {
-			return "login";
-		} else if(strPath.equals("newbie")) {
-			return "newbie";
-		} else {
-			return "choose";
-		}
+		return "home";
 	}
 	
 	@RequestMapping("/newbie")
-	public String newbie(HttpServletRequest hsr, Model model) {
-		String strPath = hsr.getParameter("path");
-		if(strPath.equals("login")) {
-			return "checknewbie";
-		} else if(strPath.equals("choose")) {
-			return "choose";
+	public String getInfo(Locale locale, Model model) {
+		return "newbie";
+	}
+	
+	@RequestMapping(value="/check_user", method = RequestMethod.POST)
+	public String check_user(HttpServletRequest hsr, Model model) {
+		String userid=hsr.getParameter("userid");
+		String userpw=hsr.getParameter("userpw");
+		
+		HttpSession session=hsr.getSession();
+		session.setAttribute("loginid", userid);
+		
+		return "redirect:/booking";
+		
+	}
+	
+	@RequestMapping(value="/booking",method=RequestMethod.GET)
+	public String booking(HttpServletRequest hsr) {
+		HttpSession session = hsr.getSession();
+		if(session.getAttribute("loginid") == null) {
+			return "redirect:/";
 		} else {
-			return "choose";
+			return "booking";			
 		}
 	}
 	
-	@RequestMapping("/check")
-	public String check(HttpServletRequest hsr, Model model) {
-		String strPath = hsr.getParameter("path");
-		if(strPath.equals("login")) {
-			return "login";
-		} else if(strPath.equals("choose")) {
-			return "newbie";
-		} else {
-			return "choose";
+	@RequestMapping("/login")
+	   public String doInfo(HttpServletRequest hsr, Model model) {
+	      String strPath = hsr.getParameter("path");
+	      if(strPath.equals("login")) {
+	         return "booking";
+	      } else if(strPath.equals("newbie")) {
+	         return "newbie";
+	      } else {
+	         return "home";
+	      }
+	   }
+	
+	@RequestMapping("/room")
+	public String room(HttpServletRequest hsr, Model model) {
+		HttpSession session = hsr.getSession();
+		if(session.getAttribute("loginid") == null) {
+			return "redirect:/";
 		}
+		//여기서 interface 호출, 결과를 room.jsp에 전달
+		iRoom room = sqlSession.getMapper(iRoom.class);
+		ArrayList<Roominfo> roominfo = room.getRoomList();
+		System.out.println(roominfo);
+		model.addAttribute("list", roominfo);
+		return "room";
 	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest hsr) {
+		HttpSession session=hsr.getSession();
+		session.invalidate();
+		return "redirect:/";
+	}
+	
 }
