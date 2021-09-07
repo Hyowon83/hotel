195216dfeb20ee8ec5b4b92bef3672e44443ec1e
Hyowon83 <aile1383@gmail.com>
2021-09-07 21:04:15 +0900

    파라미터를 받는 배열 추가(예약된 객실 베열)

--- hotel/src/main/java/com/hotel/app/iBooking.java
+++ hotel/src/main/java/com/hotel/app/iBooking.java
@@ -3,11 +3,9 @@ package com.hotel.app;
 import java.util.ArrayList;
 
 public interface iBooking {
-	ArrayList<Bookinfo> getBookList();
-	ArrayList<Bookinfo> bookedRoomList();
+	ArrayList<Bookinfo> getBookList(String checkin, String checkout);
+	ArrayList<Bookinfo> bookedRoomList(String checkin, String checkout);
 	void doDeleteBook(int bookcode);
-	void doDeleteBooking1();
-	void getAbleBookList(String typename);
 	void doAddBook(int roomcode, int person, String checkin, String checkout, String name, String mobile);
 	void doUpdateBook(int roomcode, int person, String checkin, String checkout, String name, String mobile);
 }
